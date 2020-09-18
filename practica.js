89a635f328db87d0075dfcd9d531c1a67f3fc93b
Malejandraib <mariaibanez07@gmail.com>
2020-09-17 20:11:16 -0500

    clean code

--- practica.js
+++ practica.js
@@ -1,75 +1,95 @@
 const fs = require('fs');
 const path = require('path');
 const MarkdownIt = require('markdown-it');
+const { resolve } = require('path');
+const { rejects } = require('assert');
 const md = new MarkdownIt();
 const file = './README.md'
 const asset ='./assets'
 const linkify = md.linkify
+const newPath = 'C:/Users/ASUS/Desktop/nuevo/Prueba'
+const relativePath = 'C:\\Users\\ASUS\\Desktop\\nuevo\\Prueba' //'.'
 
 // Aqui deberia resolver si el path es absoluto y normalizarlo 
 const findPath = (lookingPath)=>{
     if (!path.isAbsolute(lookingPath)){
         console.log('here is a relative path ' + lookingPath)
-        return path.resolve(path.normalize(lookingPath)) 
+        return path.resolve(lookingPath)
     }
-    return path.isAbsolute(lookingPath); // true
+    console.log(lookingPath)
+    return lookingPath; // true
 }
-console.log('this path is absolute: ' + findPath('.'))
+console.log('this path is absolute: ' + findPath(relativePath))
 
+//Aquí debería leer los directorios hasta llegar a los archivos .md
 
-const readingFolder = (pathPrueba) => {
+const busqueda = (pathPrueba, resultado)=> {   //passsing directoryPath and callback function
+    let arrResult = [];
 
-    const assets = fs.statSync(pathPrueba)  //Verifica sí es un directorio o un file
-    console.log('Here is a file: '+ assets.isFile());
+    let algoTemporal = fs.lstatSync(pathPrueba)
 
-    if (!assets.isFile()){
-        let directoryPath = path.join(pathPrueba);
-        console.log(directoryPath)
-        let findDir = []
-        fs.readdir(directoryPath, (err, content) => {   //passsing directoryPath and callback function
-            if (err){  
-                return console.log('Unable to scan directory: ' + err);
-            }
-            content.map(lookingDir =>{
-                fs.lstat(path.join(pathPrueba, lookingDir), (err,stats) => {
-                    if (err){  
-                        return console.log('Unable to scan directory: ' + err);
-                    }
-                    if(stats.isDirectory()){
-                        console.log(path.join(probAsset, lookingDir))
-                        findDir.push(path.join(probAsset, lookingDir))
-                        //readingFolder(path.join(probAsset, lookingDir))
-                        console.log(findDir)
-                        //return findDir
-                    }
-                    if(!stats.isDirectory()){
-                        let arrayFiles = content.filter(filesFold =>{
-                            if (path.extname(filesFold) =='.md') {
-                                return filesFold
-                            }
-                        });
-                        console.log(arrayFiles)
-                    }
-                
-                })
-            
-            })
-
-
-/*             else {
-                let arrayFiles = content.filter(filesFold =>{
-                    if (path.extname(filesFold) =='.md') {
-                        return filesFold
-                    }
-                });
-                console.log(arrayFiles)
-                //return arrayFiles
-            } */
-        });
+    if(algoTemporal.isDirectory()){
+        let arrArchivos = fs.readdirSync(pathPrueba)
+        arrResult = arrArchivos.map(filePath =>{
+            return busqueda(path.join(pathPrueba, filePath))
+        })
     }
+
+    else if (path.extname(pathPrueba) =='.md') {
+        let file = pathPrueba
+        arrResult.push(file)
+    } 
+
+    if (resultado){
+        resultado(arrResult.flat())
+    }
+    else{
+        return arrResult.flat()
+    }
+    
 }
 
-readingFolder(asset)
+
+
+
+const readingFolder = (pathPrueba, searchMethod) => {
+        return new Promise ((resolve, reject) => {
+            searchMethod(pathPrueba, resolve)
+        })
+}
+
+readingFolder(findPath(relativePath), busqueda).then(console.log)
+
+
+/* const readingFiles = (filePath)=>{
+    fs.createReadStream(filePath, { encoding: 'utf8' })
+    readStream.on('data', (chunk) => {
+        console.log(chunk)
+    });
+}
+
+readingFiles () */
+
+
+
+
+
+
+
+/* 
+findPath(relativePath)
+.then(readingFolder(response) {
+    return findfiles(response);
+}).then()
+
+firstRequest()
+  .then(function(response) {
+    return secondRequest(response);
+}).then(function(nextResponse) {  
+    return thirdRequest(nextResponse);
+}).then(function(finalResponse) {  
+    console.log('Final response: ' + finalResponse);
+}).catch(failureCallback); */
 
        /*      let i = 0
             while (i <= findDir.length){
